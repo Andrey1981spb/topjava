c7b519c6a32c14f2d9ab7e7c2f31574cd11cbeab
Andrey1981spb <andrey1981spb@gmail.com>
2019-11-10 20:53:26 +0300

    4 9 add hsqldb

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -13,7 +13,7 @@ CREATE TABLE users
   password         VARCHAR                 NOT NULL,
   registered       TIMESTAMP DEFAULT now() NOT NULL,
   enabled          BOOL DEFAULT TRUE       NOT NULL,
-  calories_per_day INTEGER DEFAULT 2000    NOT NULL
+  calories_per_day INTEGER DEFAULT 2000    NOT NULL,
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
