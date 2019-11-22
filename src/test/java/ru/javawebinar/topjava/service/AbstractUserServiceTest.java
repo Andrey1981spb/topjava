1adde95e242e96d63e1470506697b0181462d204
Andrey1981spb <andrey1981spb@gmail.com>
2019-11-22 13:28:47 +0300

    7 04 HW6 optional add role

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -65,15 +65,21 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
     }
 
     @Test
-    public void get() throws Exception {
-        User user = service.get(USER_ID);
-        assertMatch(user, USER);
+    void get() throws Exception {
+        User user = service.get(ADMIN_ID);
+        assertMatch(user, ADMIN);
     }
 
     @Test
-    public void getByEmail() throws Exception {
-        User user = service.getByEmail("user@yandex.ru");
-        assertMatch(user, USER);
+    void getNotFound() throws Exception {
+        assertThrows(NotFoundException.class, () ->
+                service.get(1));
+    }
+
+    @Test
+    void getByEmail() throws Exception {
+        User user = service.getByEmail("admin@gmail.com");
+        assertMatch(user, ADMIN);
     }
 
     @Test
